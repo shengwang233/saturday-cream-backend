0adec6841886440a3c4ea0fe120ef331380025f7
Sheng <wang283@sheridancollege.ca>
2025-09-19 14:05:28 -0400

    fix: insert setmealdish insertBatch

--- saturday-server/src/main/java/com/saturday/controller/admin/SetMealController.java
+++ saturday-server/src/main/java/com/saturday/controller/admin/SetMealController.java
@@ -5,8 +5,7 @@ import com.saturday.result.Result;
 import com.saturday.service.SetMealService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/admin/setmeal")
@@ -17,7 +16,8 @@ public class SetMealController {
      * Add new set meal
      */
 
-    public Result save(SetmealDTO setmealDTO){
+    @PostMapping
+    public Result save(@RequestBody SetmealDTO setmealDTO){
         setMealService.saveWithDish(setmealDTO);
         return Result.success();
     }
